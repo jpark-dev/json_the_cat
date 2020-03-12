8a5133ad23d948c7ffe2b6faf476addf248b4d00
Junsung Park <zeipar@gmail.com>
2020-03-12 22:46:41 +0000

    refactored and tested with mocha for validation

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
 const request = require('request');
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-const arg = process.argv.slice(2);
+    if (error !== null) {
+      callback(error, body);
+    } else {
+      const desc = JSON.parse(body);
+      if (desc.status !== undefined) {
+        callback(desc);
+      } else if (desc[0] === undefined) {
+        callback(error, 'Breed does not exist!');
+      } else {
+        // replace newline characters == .replace(/[\r\n]+/g, '\n');
+        callback(error, desc[0].description.trim());
+      }
+    }
 
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg[0]}`, (error, response, body) => {
-  if (error) {
-    return console.log('error:', error); // Print the error if one occurred
-  }
-  
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    return console.log(`no specific breed found: ${arg[0]}`);
-  }
-  for (let info in data) {
-    return console.log(data[info]);
-  }
-});
-
+module.exports = { fetchBreedDescription };
